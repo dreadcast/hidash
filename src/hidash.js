8355a43eebf204ab0070b0db40a8affe739feef7
Raphaël Gully <rg@kubity.com>
2015-01-12 08:59:59 +0100

    bower points to ./src

--- src/hidash.js
+++ src/hidash.js
@@ -170,6 +170,31 @@ _.mixin({
 		return false;
 	},
 	
+	closest: function(obj, number){		
+		if((current = obj.length) < 2)
+			return l - 1;
+			
+		for(var current, previous = Math.abs(obj[--current] - number); current--;)
+			if(previous < (previous = Math.abs(obj[current] - number)))
+				break;
+				
+		return obj[current + 1];
+	
+		var closest = -1,
+			prev = Math.abs(obj[0] - number);
+		
+		for (var i = 1; i < obj.length; i++){
+			var diff = Math.abs(obj[i] - number);
+			
+			if (diff <= prev){
+				prev = diff;
+				closest = obj[i];
+			}
+		}
+		
+		return closest;
+	},
+	
 	merge: function(){
 		var obj1 = arguments[0];
 		
