250e19a55d327aa05271b890e534dfa06fcc5310
Raphaël Gully <rg@kubity.com>
2015-01-12 09:02:58 +0100

    add closest

--- src/hidash.js
+++ src/hidash.js
@@ -273,6 +273,30 @@
 			loop.call(arr);
 			
 			return arr;
+		},
+		closest: function(obj, number){		
+			if((current = obj.length) < 2)
+				return l - 1;
+				
+			for(var current, previous = Math.abs(obj[--current] - number); current--;)
+				if(previous < (previous = Math.abs(obj[current] - number)))
+					break;
+					
+			return obj[current + 1];
+		
+			var closest = -1,
+				prev = Math.abs(obj[0] - number);
+			
+			for (var i = 1; i < obj.length; i++){
+				var diff = Math.abs(obj[i] - number);
+				
+				if (diff <= prev){
+					prev = diff;
+					closest = obj[i];
+				}
+			}
+			
+			return closest;
 		}
 	});
 })(_);
