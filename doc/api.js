75a0f98e3f9e2d31ebf9a118f028b99b6fa34be8
Raphaël Gully <raphael.gully@kubity.com>
2015-03-05 14:17:27 +0100

    first doc

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Hidash"
+    ],
+    "modules": [
+        "Lodash"
+    ],
+    "allModules": [
+        {
+            "displayName": "Lodash",
+            "name": "Lodash",
+            "description": "Useful mixins for Lodash"
+        }
+    ]
+} };
+});
