fe611186f54205a3866b7b2423f35b6a5b0fb793
Raphaël Gully <raphael.gully@kubity.com>
2015-03-05 16:21:28 +0100

    Grrrr !!!

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	git = require('gulp-git'),
 	jeditor = require('gulp-json-editor'),
+	bump = require('gulp-bump'),
 	_ = require('./src/hidash.js');
 
 gulp.task('scripts', function(cb){
@@ -13,11 +14,12 @@ gulp.task('scripts', function(cb){
 });
 
 // Version
-var version;
+var files = ['./package.json', './bower.json', './yuidoc.json'],
+	version;
 gulp.task('setVersion', function(){
 	version = process.argv[4];
 	
-	gulp.src('./**.json')
+	gulp.src(files)
 		.pipe(jeditor({
 			version: version
 		}))
@@ -25,17 +27,54 @@ gulp.task('setVersion', function(){
 });
 
 gulp.task('commitVersion', ['setVersion'], function(){
-	gulp.src('./**.json')
+	gulp.src(files)
 		.pipe(git.add())
 		.pipe(git.commit('Changed version to ' + version));
 });
 
-gulp.task('tagVersion', ['commitVersion'], function(){
+gulp.task('version', ['commitVersion'], function(){
+	git.tag(version, 'Changed version to ' + version);
+});
+/*
+gulp.task('version', function(){
+	version = process.argv[4];
+	
 	gulp.src('./**.json')
-		.pipe(git.tag(version, 'Changed version to ' + version));
+		.pipe(jeditor({
+			version: version
+		}))
+		.pipe(gulp.dest('.'))
+		.pipe(git.add())
+		.pipe(git.commit('Changed version to ' + version))
+		.pipe(git.tag(version, 'Changed version to ' + version))
+});
+*/
+/*
+var bumpType;
+
+gulp.task('bump', function () {
+	bumpType = process.argv[4];
+	
+	return gulp.src(['./package.json', './bower.json', './yuidoc.json'])
+		.pipe(bump({
+			type: bumpType
+		}))
+		.pipe(gulp.dest('./'));
 });
 
-gulp.task('version', ['setVersion', 'commitVersion', 'tagVersion']);
+gulp.task('tag', ['bump'], function () {
+	var pkg = require('./package.json'),
+		v = 'v' + pkg.version,
+		message = 'Release ' + v;
+	
+	return gulp.src('./')
+		.pipe(git.commit(message))
+		.pipe(git.tag(v, message))
+		.pipe(git.push('origin', 'with-gulp', '--tags'))
+		.pipe(gulp.dest('./'));
+});
+*/
+
 
 
 
@@ -55,4 +94,4 @@ gulp.task('watch', function() {
   gulp.watch('src/hidash.js', ['scripts', 'doc']);
 });
 
-gulp.task('default', ['watch', 'scripts', 'doc']);
+//gulp.task('default', ['watch', 'scripts', 'doc']);
